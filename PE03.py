58c9e44fa6b4f24ad9c58e7bdb330300467ca4a1
Damini Satya <daminisatya@gmail.com>
2016-04-19 21:15:24 +0530

    Update PE03.py

--- PE03.py
+++ PE03.py
@@ -1,22 +1,7 @@
-import eulerlib
-def compute():
-	n = 600851475143
-	while True:
-		p = smallest_prime_factor(n)
-		if p < n:
-			n //= p
-		else:
-			return str(n)
-
-
-# Returns the smallest factor of n, which is in the range [2, n]. The result is always prime.
-def smallest_prime_factor(n):
-	assert n >= 2
-	for i in range(2, eulerlib.sqrt(n) + 1):
-		if n % i == 0:
-			return i
-	return n  # n itself is prime
-
-
-if __name__ == "__main__":
-	print(compute())
+n = 600851475143
+i = 2
+while i * i < n:
+    while n % i == 0:
+        n = n / i
+    i = i + 1
+print n
