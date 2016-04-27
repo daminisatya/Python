88b6f94eb0a2e1dcf2c26964c3538ded58046f2e
Damini Satya <daminisatya@gmail.com>
2016-04-27 21:58:07 +0530

    Create PE29.py

---
+++ PE29.py
@@ -0,0 +1,9 @@
+terms = {}
+count = 0
+for a in xrange(2,101):
+    for b in xrange(2,101):
+        c = pow(a,b)
+        if not terms.get(c, 0):
+            terms[c] = 1
+            count = count + 1
+print count
