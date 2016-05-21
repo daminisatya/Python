3e26fec5ee376b637470b3f8e93bdb2b0a037f6b
daminisatya <daminisatya@gmail.com>
2016-05-21 20:30:19 +0530

    Removed code

--- PE42.py
+++
@@ -1,6 +0,0 @@
-def worth(word): return sum(ord(letter) - ord('A') + 1 for letter in word)
-
-words = open('words.txt').read().replace('"', '').split(',')
-triangle_numbers = dict.fromkeys(list(n*(n+1)/2 for n in xrange(1, 100)), 1)
-
-print sum(1 for word in words if worth(word) in triangle_numbers)
