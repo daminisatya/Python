dc71d6ddc935918a3ebadb1aa732225a4b61bbf0
daminisatya <daminisatya@gmail.com>
2016-06-18 13:41:41 +0530

    deleted

--- PE21.py
+++
@@ -1,3 +0,0 @@
-def divisors(n): return list(i for i in xrange(1, n/2+1) if n % i == 0)
-pair = dict( ((n, sum(divisors(n))) for n in xrange(1, 10000)) )
-print sum(n for n in xrange(1, 10000) if pair.get(pair[n], 0) == n and pair[n] != n)
