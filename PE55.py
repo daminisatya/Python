842c6ef567b39a8e2e9c49019fd9598dedf76c04
daminisatya <daminisatya@gmail.com>
2016-08-18 16:12:47 +0530

    removed code

--- PE55.py
+++
@@ -1,9 +0,0 @@
-
-def is_lychrel(n):
-    n = str(n)
-    for count in xrange(0, 50):
-        n = str(int(n) + int(n[::-1]))
-        if n == n[::-1]: return False
-    return True
-
-print sum(1 for n in xrange(0, 10000) if is_lychrel(n))
